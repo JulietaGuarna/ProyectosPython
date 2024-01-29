f89f88b8408745e96264ffda13bc10f6044e6ab8
JulietaGuarna <103537132+JulietaGuarna@users.noreply.github.com>
2024-01-28 21:37:25 -0300

    agregar if/else palabras_locas

--- index.py
+++ index.py
@@ -1,16 +1,11 @@
-# organización = "freeCodeCamp"
-
-#print("Aprende a programar con " + organización) 
-#print("Aprende a programar con {}" .format(organización))
-#print(f"Aprende a programar con {organización}")
-
 #Mad Lips (Palabras Locas)
 
 adj = input("Adejetivo: ")
 verbo1 = input("Verbo: ")
 verbo2 = input("Verbo: ")
 sustantivo_plural = input("Sustantivo Plural: ")
+muerte = "pero es sujetada justo antes por su hermano" if  input("Final feliz?(Si/No): ").upper() == "SI" else "muriendo mientras su hermano escapa"
 
-madlib = f"La princesa {adj}, siempre esta {verbo1} y busca problemas. Su hermano, el principe, siempre esta {verbo2} e intenta resolver sus {sustantivo_plural}"
+madlib = f"La princesa {adj}, siempre esta {verbo1} y busca problemas. Su hermano, el principe, siempre esta {verbo2} e intenta resolver sus {sustantivo_plural}. Corriendo por la montaña, los hermanos, tropiezan con las rocas. La princesa cae al precipicio {muerte}"
 
 print(madlib)
