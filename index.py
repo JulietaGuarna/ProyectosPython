69d3afd15a26853c6173c42d713f015c85dde50a
JulietaGuarna <103537132+JulietaGuarna@users.noreply.github.com>
2024-01-28 21:04:56 -0300

    palabras locas terminado

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
-print("adivinar_numero")
+# organización = "freeCodeCamp"
+
+#print("Aprende a programar con " + organización) 
+#print("Aprende a programar con {}" .format(organización))
+#print(f"Aprende a programar con {organización}")
+
+#Mad Lips (Palabras Locas)
+
+adj = input("Adejetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo Plural: ")
+
+madlib = f"La princesa {adj}, siempre esta {verbo1} y busca problemas. Su hermano, el principe, siempre esta {verbo2} e intenta resolver sus {sustantivo_plural}"
+
+print(madlib)
